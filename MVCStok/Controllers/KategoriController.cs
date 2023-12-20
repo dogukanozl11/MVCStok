c93ecc0b0980eaaaf9076df0ac210e840a632287
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-12-20 23:19:03 +0300

    Add project files.

---
+++ MVCStok/Controllers/KategoriController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStok.Models.Entity;
+
+namespace MVCStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        [HttpGet]// sayfa içinde bi işlem yapılmazsa sadece bunu yap.
+        public ActionResult YeniKategori() 
+        {
+            return View();
+        }
+
+        [HttpPost] //Gönderme işlemi yaparsak çalışacak.
+        public ActionResult YeniKategori(TBLKATEGORI p1)
+        {
+            db.TBLKATEGORI.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ MVCStok/Controllers/UrunController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStok.Models.Entity;
+
+namespace MVCStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degeler = (from i in db.TBLKATEGORI.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text = i.KATEGORIAD,
+                                                Value = i.KATEGORIID.ToString()
+                                            }).ToList();
+            ViewBag.dgr=degeler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLER p1)
+        {
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
